091ee469ca8aeeb7af3a3b18c6428ee2ef38e45f
pedrommaiaa <pedroceduca@gmail.com>
2021-07-02 17:01:55 -0400

    testing errors

--- tests/bash/runtests.sh
+++ tests/bash/runtests.sh
@@ -4,25 +4,63 @@
 
 cd ../
 
+# Build our compiler if needed
 if [ ! -f ../flow ]
-then echo "Need to build ../flow first!"; exit 1
+then (cd ..; make)
 fi
 
-for i in input*fl
-do if [ ! -f "results/out.$i" ]
+# Try to use each input source file
+for i in input*
+# We can't do anything if there's no file to test against
+do if [ ! -f "results/out.$i" -a ! -f "err/err.$i" ]
    then echo "Can't run test on $i, no output file!"
-   else
-     echo -n $i
-     ../flow $i
-     cc -o out out.s ../lib/printint.c
-     ./out > trial.$i
-     cmp -s "results/out.$i" "trial.$i"
-     if [ "$?" -eq "1" ]
-     then echo ": failed"
-       diff -c "results/out.$i" "trial.$i"
-       echo
-     else echo ": OK"
+
+   # Output file: compile the source, run it and
+   # capture the output, and compare it against
+   # the known-good output
+   else if [ -f "results/out.$i" ]
+        then
+	  # Print the test name, compile it
+	  # with our compiler
+          echo -n $i
+          ../flow $i
+
+	  # Assemble the output, run it
+	  # and get the output in trial.$i
+          cc -o out out.s ../lib/printint.c
+          ./out > trial.$i
+
+  	  # Compare this agains the correct output
+          cmp -s "out.$i" "trial.$i"
+
+	  # If different, announce failure
+          # and print out the difference
+          if [ "$?" -eq "1" ]
+          then echo ": failed"
+            diff -c "out.$i" "trial.$i"
+            echo
+
+	  # No failure, so announce success
+          else echo ": OK"
+          fi
+
+   # Error file: compile the source and
+   # capture the error messages. Compare
+   # against the known-bad output. Same
+   # mechanism as before
+   else if [ -f "err/err.$i" ]
+        then
+          echo -n $i
+          ../flow $i 2> "trial.$i"
+          cmp -s "err.$i" "trial.$i"
+          if [ "$?" -eq "1" ]
+          then echo ": failed"
+            diff -c "err/err.$i" "trial.$i"
+            echo
+          else echo ": OK"
+          fi
+        fi
      fi
-     rm -f out out.s "trial.$i"
    fi
+   rm -f out out.s "trial.$i"
 done
